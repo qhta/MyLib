5604f07b4092053166d1968c69791117d1bff8e9
qhta <qhta@eti.pg.gda.pl>
2018-04-21 17:44:16 +0200

    Some libs moved to namespace MyLib

--- MyLib.TypeUtils/EnumTypeConverter.cs
+++ MyLib.TypeUtils/EnumTypeConverter.cs
@@ -12,13 +12,13 @@ namespace MyLib.TypeUtils
   public static class EnumTypeConverter
   {
     class Dicts {
-      public Dictionary<UInt64, string> ValueToString;
-      public Dictionary<string, UInt64> StringToValue;
+      public ConcurrentDictionary<UInt64, string> ValueToString;
+      public ConcurrentDictionary<string, UInt64> StringToValue;
 
       public Dicts()
       {
-        ValueToString = new Dictionary<UInt64, string>();
-        StringToValue = new Dictionary<string, UInt64>();
+        ValueToString = new ConcurrentDictionary<UInt64, string>();
+        StringToValue = new ConcurrentDictionary<string, UInt64>();
       }
     }
 
@@ -31,73 +31,76 @@ namespace MyLib.TypeUtils
         if (convDict.ContainsKey(type))
           return;
         var uType = Enum.GetUnderlyingType(type);
-        var Dicts = new Dicts();
-        convDict.TryAdd(type, Dicts);
-        foreach (var enumMember in type.GetFields(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic))
+        var dicts = new Dicts();
+        dicts = convDict.GetOrAdd(type, dicts);
+        lock (dicts)
         {
+          foreach (var enumMember in type.GetFields(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic))
+          {
 
-          UInt64 value = 0;
-          var obj = enumMember.GetValue(null);
-          if (uType==typeof(byte))
-            value = (byte)obj;
-          else if (uType==typeof(sbyte))
-            value = (UInt64)(sbyte)obj;
-          else if (uType==typeof(short))
-            value = (UInt64)(short)obj;
-          else if (uType==typeof(ushort))
-            value = (ushort)obj;
-          else if (uType==typeof(uint))
-            value = (UInt64)(uint)obj;
-          else if (uType==typeof(Int64))
-            value = (UInt64)(Int64)obj;
-          else if (uType==typeof(UInt64))
-            value = (UInt64)obj;
-          else
-            value = (UInt64)(int)obj;
-          //if (enumMember.Name=="Interjection")
-          //  Debug.Assert(true);
-          var enumMemberAttribute = enumMember.GetCustomAttributes(typeof(EnumMemberAttribute), false).FirstOrDefault() as EnumMemberAttribute;
-          if (enumMemberAttribute!=null)
-          {
-            Dicts.ValueToString.Add(value, enumMemberAttribute.Value);
-            string s = enumMemberAttribute.Value.ToLower();
-            Dicts.StringToValue.Add(s, value);
-            if (s!=enumMember.Name.ToLower())
-              Dicts.StringToValue.Add(enumMember.Name.ToLower(), value);
+            UInt64 value = 0;
+            var obj = enumMember.GetValue(null);
+            if (uType==typeof(byte))
+              value = (byte)obj;
+            else if (uType==typeof(sbyte))
+              value = (UInt64)(sbyte)obj;
+            else if (uType==typeof(short))
+              value = (UInt64)(short)obj;
+            else if (uType==typeof(ushort))
+              value = (ushort)obj;
+            else if (uType==typeof(uint))
+              value = (UInt64)(uint)obj;
+            else if (uType==typeof(Int64))
+              value = (UInt64)(Int64)obj;
+            else if (uType==typeof(UInt64))
+              value = (UInt64)obj;
+            else
+              value = (UInt64)(int)obj;
+            //if (enumMember.Name=="Interjection")
+            //  Debug.Assert(true);
+            var enumMemberAttribute = enumMember.GetCustomAttributes(typeof(EnumMemberAttribute), false).FirstOrDefault() as EnumMemberAttribute;
+            if (enumMemberAttribute!=null)
+            {
+              dicts.ValueToString.TryAdd(value, enumMemberAttribute.Value);
+              string s = enumMemberAttribute.Value.ToLower();
+              dicts.StringToValue.TryAdd(s, value);
+              if (s!=enumMember.Name.ToLower())
+                dicts.StringToValue.TryAdd(enumMember.Name.ToLower(), value);
+            }
+            else
+            {
+              dicts.ValueToString.TryAdd(value, enumMember.Name);
+              dicts.StringToValue.TryAdd(enumMember.Name.ToLower(), value);
+            }
           }
-          else
-          {
-            Dicts.ValueToString.Add(value, enumMember.Name);
-            Dicts.StringToValue.Add(enumMember.Name.ToLower(), value);
-          }
-        }
-        var strings = Dicts.StringToValue.ToList();
+        var strings = dicts.StringToValue.ToList();
         var sortedStrings = new SortedSet<string>(strings.Select(item => item.Key).ToList());
-        foreach (var kvp in strings)
-        {
-          string str = kvp.Key;
-          //if (str.StartsWith("colloq"))
-          //  Debug.Assert(true);
-          string s = str;
-          while (s.Length>0)
+          foreach (var kvp in strings)
           {
-            if (!sortedStrings.Contains(s))
+            string str = kvp.Key;
+            //if (str.StartsWith("colloq"))
+            //  Debug.Assert(true);
+            string s = str;
+            while (s.Length>0)
             {
-              //if ( && strings.Select(item => item.Key).Where(item => item.StartsWith(s)).Count()<=1))
-              sortedStrings.Add(s);
-              Dicts.StringToValue.Add(s, kvp.Value);
+              if (!sortedStrings.Contains(s))
+              {
+                //if ( && strings.Select(item => item.Key).Where(item => item.StartsWith(s)).Count()<=1))
+                sortedStrings.Add(s);
+                dicts.StringToValue.TryAdd(s, kvp.Value);
+              }
+              s = s.Substring(0, s.Length-1);
             }
-            s = s.Substring(0, s.Length-1);
-          }
-          s = str.RemoveAll(1, new char[] { 'a', 'e', 'i', 'o', 'u', 'y' });
-          while (s.Length>0)
-          {
-            if (!sortedStrings.Contains(s) && strings.Select(item => item.Key).Where(item => item.StartsWith(s)).Count()<=1)
+            s = str.RemoveAll(1, new char[] { 'a', 'e', 'i', 'o', 'u', 'y' });
+            while (s.Length>0)
             {
-              sortedStrings.Add(s);
-              Dicts.StringToValue.Add(s, kvp.Value);
+              if (!sortedStrings.Contains(s) && strings.Select(item => item.Key).Where(item => item.StartsWith(s)).Count()<=1)
+              {
+                sortedStrings.Add(s);
+                dicts.StringToValue.TryAdd(s, kvp.Value);
+              }
+              s = s.Substring(0, s.Length-1);
             }
-            s = s.Substring(0, s.Length-1);
           }
         }
       }
@@ -159,19 +162,22 @@ namespace MyLib.TypeUtils
       invalidKey = null;
       if (!convDict.ContainsKey(typeof(EType)))
         CreateDicts(typeof(EType));
-      string[] ss = value.Split(new char[] { ';', ',', '|', '+' });
-      UInt64 n = 0;
-      foreach (var s in ss)
+      lock (convDict[typeof(EType)])
       {
-        if (!convDict[typeof(EType)].StringToValue.TryGetValue(s.ToLower().Trim(), out UInt64 v))
+        string[] ss = value.Split(new char[] { ';', ',', '|', '+' });
+        UInt64 n = 0;
+        foreach (var s in ss)
         {
-          invalidKey = s;
-          return false;
+          if (!convDict[typeof(EType)].StringToValue.TryGetValue(s.ToLower().Trim(), out UInt64 v))
+          {
+            invalidKey = s;
+            return false;
+          }
+          n |= v;
         }
-        n |= v;
-      }
       result = (EType)Enum.ToObject(typeof(EType), n);
       return true;
+      }
     }
   }
 }
