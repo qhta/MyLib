cdc01a7800943077e34bc574b367f3be0a782778
qhta <qhta@eti.pg.gda.pl>
2018-05-07 10:18:53 +0200

    MVVM DataViewModels added

---
+++ MyLib.MVVM.DataViewModels/DataRowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib.MVVM
+{
+  public class DataRowViewModel: ViewModel
+  {
+    public virtual bool HasRowDetails => false;
+  }
+}
---
+++ MyLib.MVVM.DataViewModels/DataSetViewModel`1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyLib.MultiThreadingObjects;
+
+namespace MyLib.MVVM
+{
+  public abstract class DataSetViewModel<EntityType>: DataSetViewModel
+  {
+    public DispatchedCollection<EntityType> Items => _Items;
+    DispatchedCollection<EntityType> _Items = new DispatchedCollection<EntityType>();
+
+  }
+}
