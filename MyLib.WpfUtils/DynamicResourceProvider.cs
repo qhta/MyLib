098d89ec02caa7ba71d3169e0805e5d6253bd6f1
qhta <qhta@eti.pg.gda.pl>
2018-06-27 14:49:42 +0200

    DynamicResource MarkupExtension implemented

---
+++ MyLib.WpfUtils/DynamicResourceProvider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace MyLib.WpfUtils
+{
+  public class DynamicResourceProvider : MarkupExtension
+  {
+    public DynamicResourceProvider(string typeName)
+    {
+      TypeName = typeName;
+    }
+    public string TypeName { get; set; }
+
+    public override object ProvideValue(IServiceProvider serviceProvider)
+    {
+      if (serviceProvider == null)
+      {
+        throw new ArgumentNullException("serviceProvider");
+      }
+      IXamlTypeResolver xamlTypeResolver = serviceProvider.GetService(typeof(IXamlTypeResolver)) as IXamlTypeResolver;
+      if (xamlTypeResolver == null)
+      {
+        throw new ArgumentException("IXamlTypeResolver");
+      }
+      Type type = xamlTypeResolver.Resolve(this.TypeName);
+      return type.GetConstructor(new Type[] { }).Invoke(new object[] { });
+    }
+
+  }
+}
