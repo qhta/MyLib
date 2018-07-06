1ccbed6c8995ba67bcd7ff39525eea526d0eb016
qhta <qhta@eti.pg.gda.pl>
2018-07-06 13:51:46 +0200

    FloatDoubleFormater added

---
+++ MyLib.WpfUtils/FloatDoubleFormater.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MyLib.WpfUtils
+{
+  public class FloatDoubleFormater : IValueConverter
+  {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      double x = (double)value;
+      return x.ToString((string)parameter, culture);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
