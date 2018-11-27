55026506d79a3baa698a7b990620dd344f960ff4
qhta <qhta@eti.pg.gda.pl>
2018-11-27 23:08:31 +0100

    ColorPad AlphaSlider & ProbeRectangle added

---
+++ Qhta.WPF.Utils/FrameworkElementUtils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Qhta.WPF.Utils
+{
+  public static class FrameworkElementUtils
+  {
+
+    public static Window FindParentWindow(this FrameworkElement element)
+    {
+      object parent = element.Parent;
+      while (parent != null && !(parent is Window))
+        parent = (parent as FrameworkElement).Parent;
+      return parent as Window;
+    }
+  }
+}
