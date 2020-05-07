30109aa86cd97c0c632bea92f29c601fed9bf2a7
qhta <qhta@eti.pg.gda.pl>
2020-05-07 18:24:23 +0200

    Packages updated

---
+++ Qhta.MVVM/DispatchedCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace Qhta.MVVM
+{
+  public abstract class DispatchedCommand: ICommand
+  {
+    public DispatchedCommand() { }
+
+    public DispatchedCommand(Dispatcher dispatcher)
+    {
+      Dispatcher = dispatcher;
+    }
+
+    public Dispatcher Dispatcher { get; set; }
+
+    public event EventHandler CanExecuteChanged;
+
+    public void NotifyCanExecuteChanged()
+    {
+      if (CanExecuteChanged != null)
+      {
+        if (Dispatcher != null)
+          Dispatcher.Invoke(() =>
+          {
+            CanExecuteChanged.Invoke(this, new EventArgs());
+          });
+        else
+          CanExecuteChanged.Invoke(this, new EventArgs());
+      }
+    }
+
+    public abstract bool CanExecute(object parameter);
+
+    public abstract void Execute(object parameter);
+
+  }
+}
