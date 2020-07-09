2d2660ad978bbd802e2680034101a65906112409
qhta <qhta@eti.pg.gda.pl>
2020-07-09 18:47:31 +0200

    FormatHtmlForClipboard introduced

--- Qhta.HtmlUtils/HtmlTextUtils.cs
+++ Qhta.HtmlUtils/HtmlTextUtils.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using HtmlAgilityPack;
@@ -180,5 +181,45 @@ namespace Qhta.HtmlUtils
       }
       return str.Trim();
     }
+
+    public static object FormatHtmlForClipboard(string htmlFragment)
+    {
+      string headerFormat
+        = "Version:0.9\r\nStartHTML:{0:000000}\r\nEndHTML:{1:000000}"
+        + "\r\nStartFragment:{2:000000}\r\nEndFragment:{3:000000}\r\n";
+
+      string htmlHeader
+        = "<html>\r\n<head>\r\n"
+        + "<meta http-equiv=\"Content-Type\""
+        + " content=\"text/html; charset=utf-8\">\r\n"
+        + "<title>HTML clipboard</title>\r\n</head>\r\n<body>\r\n"
+        + "<!--StartFragment-->";
+
+      string htmlFooter = "<!--EndFragment-->\r\n</body>\r\n</html>\r\n";
+      string headerSample = String.Format(headerFormat, 0, 0, 0, 0);
+
+      Encoding encoding = Encoding.UTF8;
+      int headerSize = encoding.GetByteCount(headerSample);
+      int htmlHeaderSize = encoding.GetByteCount(htmlHeader);
+      int htmlFragmentSize = encoding.GetByteCount(htmlFragment);
+      int htmlFooterSize = encoding.GetByteCount(htmlFooter);
+
+      string htmlResult
+        = String.Format(
+            CultureInfo.InvariantCulture,
+            headerFormat,
+            /* StartHTML     */ headerSize,
+            /* EndHTML       */ headerSize + htmlHeaderSize + htmlFragmentSize + htmlFooterSize,
+            /* StartFragment */ headerSize + htmlHeaderSize,
+            /* EndFragment   */ headerSize + htmlHeaderSize + htmlFragmentSize)
+        + htmlHeader
+        + htmlFragment
+        + htmlFooter;
+      return new MemoryStream(encoding.GetBytes(htmlResult));
+      //DataObject obj = new DataObject();
+      //obj.SetData(DataFormats.Html, new MemoryStream(encoding.GetBytes(htmlResult)));
+      //Clipboard.SetDataObject(obj, true);
+    }
+
   }
 }
--- Qhta.HtmlUtils/HtmlUtils.cs
+++ Qhta.HtmlUtils/HtmlUtils.cs
@@ -31,5 +31,6 @@ namespace Qhta.HtmlUtils
       return sb.ToString();
     }
 
+
   }
 }
