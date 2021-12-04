eac978db0dd0147a673d49e69c3be39ca20a1e49
Jarosław Kuchta <qhta@eti.pg.gda.pl>
2021-12-04 21:31:54 +0100

    ErrList moved to Erratum

---
+++ Qhta.Erratum/ErrList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qhta.Erratum
+{
+  public struct ErrEntry
+  {
+    public string Exp;
+    public string Found;
+    public string Repl;
+    public int? Length;
+    public int? Skip;
+  }
+
+  public struct ErrKey
+  {
+    public string Exp;
+    public string Found;
+  }
+
+  public struct ErrVal
+  {
+    public int? Length;
+    public string Repl;
+  }
+
+  public class ErrList: List<ErrEntry>
+  {
+  }
+}
