f112a316dfa0307d6866b21042624ae85835d082
qhta <qhta@eti.pg.gda.pl>
2023-03-14 08:24:50 +0100

    ElementCollectionConverter has now Update/Compare-OpenXmlElementCollectionMethod with delegate parameters

---
+++ Qhta.DeepCompare/TypeDiff.cs
@@ -0,0 +1,74 @@
+﻿namespace Qhta.DeepCompare;
+
+/// <summary>
+/// Class representing a single difference
+/// </summary>
+public class Diff
+{
+  /// <summary>
+  /// The name of the checked object. Usually type name.
+  /// </summary>
+  public string? ObjectName { get; set; }
+
+  /// <summary>
+  /// Property name.
+  /// </summary>
+  public string? PropertyName { get; set; }
+
+  /// <summary>
+  /// Expected value.
+  /// </summary>
+  public object? ExpectedValue { get; set; }
+
+  /// <summary>
+  /// Received value.
+  /// </summary>
+  public object? ReceivedValue { get; set; }
+
+  /// <summary>
+  /// Initializes a new instance of the <see cref="Diff"/> class.
+  /// </summary>
+  /// <param name="objectName">Name of the checked object.</param>
+  /// <param name="propertyName">Name of the different property.</param>
+  /// <param name="expValue">Expected value.</param>
+  /// <param name="recValue">Received value.</param>
+  public Diff(string? objectName, string? propertyName, object? expValue, object? recValue)
+  {
+    ObjectName = objectName;
+    PropertyName = propertyName;
+    ExpectedValue = expValue;
+    ReceivedValue = recValue;
+  }
+
+  /// <summary>
+  /// Converts to string (for debugging purpose).
+  /// </summary>
+  public override string ToString()
+  {
+    var result = Concat(ObjectName, PropertyName);
+    if (ReceivedValue != null && ExpectedValue == null)
+      result += $" is {ReceivedValue} but should be null";
+    else if (ReceivedValue == null && ExpectedValue != null)
+      result += $" is null but should be {ExpectedValue}";
+    else if (ReceivedValue == null && ExpectedValue == null)
+      result += $" is null and should be null";
+    else
+      result += $" is {ReceivedValue} but should be {ExpectedValue}";
+    return result;
+  }
+
+  /// <summary>
+  /// Concats the specified object name with property name with separating dot. 
+  /// If property name is indexing clause in bracket, dot is ommited.
+  /// </summary>
+  /// <param name="objectName">Name of the object.</param>
+  /// <param name="propertyName">Name of the property.</param>
+  /// <returns></returns>
+  public static string Concat(string? objectName, string? propertyName)
+  {
+     var result = propertyName?.StartsWith("[")==true 
+      ? $"{String.Concat(objectName, propertyName)}" 
+      : $"{StringUtils.Concat2(objectName, ".", propertyName)}";
+    return result;
+  }
+}
