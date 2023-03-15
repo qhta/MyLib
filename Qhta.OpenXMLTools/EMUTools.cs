441d362ca9dbe94d6863f9396a8e317b339493e3
qhta <qhta@eti.pg.gda.pl>
2023-03-15 21:34:05 +0100

    Body elements can be sections

--- Qhta.OpenXMLTools/EMUTools.cs
+++ Qhta.OpenXMLTools/EMUTools.cs
@@ -5,23 +5,23 @@ namespace Qhta.OpenXMLTools;
 
 public static class EMUTools
 {
-  public static double ToMM(this Int32Value x)
-  {
-    return x / 56.6952380952381;
-  }
-
-  public static double ToMM(this Int32Value x, int prec)
+  public static double ToMM(this Int32Value x, int prec=0)
   {
     return Math.Round(x / 56.6952380952381, prec);
   }
 
-  public static double ToMM(this UInt32Value x)
-  {
-    return x / 56.6952380952381;
-  }
-
-  public static double ToMM(this UInt32Value x, int prec)
+  public static double ToMM(this UInt32Value x, int prec=0)
   {
     return Math.Round(x / 56.6952380952381, prec);
+  }
+
+  public static string ToStringMM(this Int32Value x, int prec=0)
+  {
+    return $"{Math.Round(x / 56.6952380952381, prec)}mm";
+  }
+
+  public static string ToStringMM(this UInt32Value x, int prec=0)
+  {
+    return $"{Math.Round(x / 56.6952380952381, prec)}mm";
   }
 }
