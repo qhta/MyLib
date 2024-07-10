c09d6a76af8a1cd9f5319f5666916ffd63ee145f
Jarosław Kuchta <qhta123@outlook.com>
2024-07-10 21:20:55 +0200

    Documentation written

--- Qhta.Access.Dao/obj/Debug/net8.0-windows/Qhta.Access.Dao.AssemblyInfo.cs
+++ Qhta.Access.Dao/obj/Debug/net8.0-windows/Qhta.Access.Dao.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Qhta.Access.Dao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8518a0b83ec679294807af9abf51ed9dada0b3f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76d35aac6af0f50949ad95f9eecd69d7bc747bbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Qhta.Access.Dao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Qhta.Access.Dao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
