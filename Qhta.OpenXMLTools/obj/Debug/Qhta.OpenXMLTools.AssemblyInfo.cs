2cbdb31b793630192e8dec6e598caa0195373034
Jarosław Kuchta <qhta123@outlook.com>
2024-07-12 12:57:15 +0200

    Members description text read

--- Qhta.OpenXMLTools/obj/Debug/Qhta.OpenXMLTools.AssemblyInfo.cs
+++ Qhta.OpenXMLTools/obj/Debug/Qhta.OpenXMLTools.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Qhta.OpenXMLTools")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c09d6a76af8a1cd9f5319f5666916ffd63ee145f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23740a4700a73b8c6bea94422d986cf34ae34584")]
 [assembly: System.Reflection.AssemblyProductAttribute("Qhta.OpenXMLTools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Qhta.OpenXMLTools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
