fadc5c772e4900f6cc8217fd690de3935a636dd3
Jarosław Kuchta <qhta123@eti.pg.gda.pl>
2024-08-16 21:11:54 +0200

    CustomToolTip implemented

--- Qhta.OpenXMLTools/obj/Debug/netstandard2.0/Qhta.OpenXMLTools.AssemblyInfo.cs
+++ Qhta.OpenXMLTools/obj/Debug/netstandard2.0/Qhta.OpenXMLTools.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("OpenXmlTools is a library for working with OpenXml documents.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0+72fe18866edf852559d15933010d96d98b5fc40b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0+bd8cf65cce176da82ed240a1a62eab438aafefd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Qhta.OpenXmlTools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Qhta.OpenXmlTools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("2.0.0.0")]
