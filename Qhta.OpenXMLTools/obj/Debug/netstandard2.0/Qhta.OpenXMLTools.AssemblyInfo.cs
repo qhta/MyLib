3b8dd659dd76af64134568a4755ced15426c4f54
Jarosław Kuchta <qhta123@eti.pg.gda.pl>
2024-08-17 14:44:39 +0200

    PropertyCaptionTemplateSelector introduced

--- Qhta.OpenXMLTools/obj/Debug/netstandard2.0/Qhta.OpenXMLTools.AssemblyInfo.cs
+++ Qhta.OpenXMLTools/obj/Debug/netstandard2.0/Qhta.OpenXMLTools.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("OpenXmlTools is a library for working with OpenXml documents.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0+47e9ed89f8a1c8b82270d5ed175a7c2b6d3aad2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0+59399ea5f605a5aab4dff5e761a178bf81a926c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Qhta.OpenXmlTools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Qhta.OpenXmlTools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("2.0.0.0")]
