43fb1f958f06dfc0d4f3440c3b339d8857713e64
Jarosław Kuchta <qhta123@eti.pg.gda.pl>
2024-08-21 14:50:28 +0200

    Lazy loading applied for DocumentPropertiesView and DocumentSettingsView

--- Qhta.OpenXMLTools/obj/Debug/netstandard2.0/Qhta.OpenXMLTools.AssemblyInfo.cs
+++ Qhta.OpenXMLTools/obj/Debug/netstandard2.0/Qhta.OpenXMLTools.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("OpenXmlTools is a library for working with OpenXml documents.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0+034c5d7712e6757fa93dae9f1bff0daff1a4b4a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0+d2df534fa1ee905a923ebed55d847d8a10522ccf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Qhta.OpenXmlTools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Qhta.OpenXmlTools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("2.0.0.0")]
