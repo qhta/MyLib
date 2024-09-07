393388a15be3ef9ae1ead371732beee6758a572b
Jarosław Kuchta <qhta123@eti.pg.gda.pl>
2024-09-07 11:15:58 +0200

    SdtElementViewModel introduced

--- Qhta.OpenXMLTools/obj/Debug/netstandard2.0/Qhta.OpenXMLTools.AssemblyInfo.cs
+++ Qhta.OpenXMLTools/obj/Debug/netstandard2.0/Qhta.OpenXMLTools.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("OpenXmlTools is a library for working with OpenXml documents.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0+d1a5db4563bb90b9c531cc8e28fa0dcee4326af8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0+8da45ee58916a092527ac4b60b8cb1b2419d658a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Qhta.OpenXmlTools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Qhta.OpenXmlTools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("2.0.0.0")]
