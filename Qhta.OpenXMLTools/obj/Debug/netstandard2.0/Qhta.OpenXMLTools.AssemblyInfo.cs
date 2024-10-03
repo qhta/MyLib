2c11baaf1df314cb54f11b772eb7de04310b9a57
Jarosław Kuchta <qhta123@eti.pg.gda.pl>
2024-10-04 00:30:38 +0200

    Late-bound Plugin Normalization introduced

--- Qhta.OpenXMLTools/obj/Debug/netstandard2.0/Qhta.OpenXMLTools.AssemblyInfo.cs
+++ Qhta.OpenXMLTools/obj/Debug/netstandard2.0/Qhta.OpenXMLTools.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("OpenXmlTools is a library for working with OpenXml documents.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0+ff4bd6c9a972a5b25f19964d011e2efe8c8495a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0+be482a9be3f4553dbbf0da9a904f31721928bdcc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Qhta.OpenXmlTools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Qhta.OpenXmlTools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("2.0.0.0")]
