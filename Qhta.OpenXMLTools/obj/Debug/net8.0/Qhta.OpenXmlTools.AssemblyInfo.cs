979c00982bad75eea0720d996ef24c2e01e2cd17
Jarosław Kuchta <qhta123@outlook.com>
2026-03-26 20:02:35 +0100

    Order in Enum types from Core

--- Qhta.OpenXMLTools/obj/Debug/net8.0/Qhta.OpenXmlTools.AssemblyInfo.cs
+++ Qhta.OpenXMLTools/obj/Debug/net8.0/Qhta.OpenXmlTools.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("OpenXmlTools is a library for working with OpenXml documents.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0+ade5451e02fde2ed384c583b3b3de6a2e8bfa801")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0+1ce5361cb37dc7b6791ce5ae73a7d73ca3e0a8fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Qhta.OpenXmlTools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Qhta.OpenXmlTools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("2.0.0.0")]
