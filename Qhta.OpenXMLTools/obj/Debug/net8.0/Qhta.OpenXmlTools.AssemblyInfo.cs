1f86810d8d9c804c0a994f7314e4ff3fd428ad09
Jarosław Kuchta <qhta123@outlook.com>
2026-03-31 00:05:18 +0200

    Interfaces interfaces now start from "I"

--- Qhta.OpenXMLTools/obj/Debug/net8.0/Qhta.OpenXmlTools.AssemblyInfo.cs
+++ Qhta.OpenXMLTools/obj/Debug/net8.0/Qhta.OpenXmlTools.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("OpenXmlTools is a library for working with OpenXml documents.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0+06e964590cbd54e8a6dd0d07e13e6ccc80a9236c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0+a9fe61c43916f09a2a362e299c78c1255e1fca09")]
 [assembly: System.Reflection.AssemblyProductAttribute("Qhta.OpenXmlTools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Qhta.OpenXmlTools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("2.0.0.0")]
