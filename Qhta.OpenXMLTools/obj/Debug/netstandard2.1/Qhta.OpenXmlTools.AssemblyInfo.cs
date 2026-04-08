8e9c2934dbab291beef79cbbd9b47ce42825def2
Jarosław Kuchta <qhta123@outlook.com>
2026-04-08 14:51:20 +0200

    ColorTypesTest passed

--- Qhta.OpenXMLTools/obj/Debug/netstandard2.1/Qhta.OpenXmlTools.AssemblyInfo.cs
+++ Qhta.OpenXMLTools/obj/Debug/netstandard2.1/Qhta.OpenXmlTools.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("OpenXmlTools is a library for working with OpenXml documents.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0+ceed4fd42cfe69eb9150978d5b5b2557ce1576c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0+8c8483f55b8a17b3cbe0c0f256aeb40fe15cb4e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Qhta.OpenXmlTools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Qhta.OpenXmlTools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("2.0.0.0")]
