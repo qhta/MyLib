7766731b8202688316d83c5b559e24c458ca6e81
Jarosław Kuchta <qhta123@outlook.com>
2026-04-10 16:19:25 +0200

    XmlRoot attributes added

--- Qhta.OpenXMLTools/obj/Debug/net8.0/Qhta.OpenXmlTools.AssemblyInfo.cs
+++ Qhta.OpenXMLTools/obj/Debug/net8.0/Qhta.OpenXmlTools.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("OpenXmlTools is a library for working with OpenXml documents.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0+604c8d48e58bfae083b6ffd9e2e70c4e975d925f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0+661836dd68d3f0874d5749c19e93219a90c78e9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Qhta.OpenXmlTools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Qhta.OpenXmlTools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("2.0.0.0")]
