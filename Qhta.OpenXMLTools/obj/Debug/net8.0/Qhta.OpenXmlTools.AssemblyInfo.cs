0c8733ec2effe31f791162e3de2d2bc71752a5b1
Jarosław Kuchta <qhta123@outlook.com>
2026-04-12 06:52:32 +0200

    ColorScheme Xml Serialization done

--- Qhta.OpenXMLTools/obj/Debug/net8.0/Qhta.OpenXmlTools.AssemblyInfo.cs
+++ Qhta.OpenXMLTools/obj/Debug/net8.0/Qhta.OpenXmlTools.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("OpenXmlTools is a library for working with OpenXml documents.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0+995eace4f3bec9fd81c46c1876a5c0709bc1e98b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0+f392656b69896c1a0cd8a0d949b8f8d910a01055")]
 [assembly: System.Reflection.AssemblyProductAttribute("Qhta.OpenXmlTools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Qhta.OpenXmlTools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("2.0.0.0")]
