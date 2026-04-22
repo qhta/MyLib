39c7c471b26f64942e5d83970f1c522a7ae4c4a4
Jarosław Kuchta <qhta123@outlook.com>
2026-04-22 20:19:09 +0200

    Paragraph Items loaded

--- Qhta.OpenXMLTools/obj/Debug/net8.0/Qhta.OpenXmlTools.AssemblyInfo.cs
+++ Qhta.OpenXMLTools/obj/Debug/net8.0/Qhta.OpenXmlTools.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("OpenXmlTools is a library for working with OpenXml documents.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0+b373570b2368a817bb5847fecc3107ba4cf1b559")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0+2b6abbaadf3f795908beaa4cf80080246b4b359e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Qhta.OpenXmlTools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Qhta.OpenXmlTools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("2.0.0.0")]
