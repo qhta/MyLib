96f5cb34628be79932586d62ee8e374f450e4260
Jarosław Kuchta <qhta123@outlook.com>
2026-04-30 15:05:52 +0200

    TestFileName specialized

--- Qhta.OpenXMLTools/obj/Debug/netstandard2.1/Qhta.OpenXmlTools.AssemblyInfo.cs
+++ Qhta.OpenXMLTools/obj/Debug/netstandard2.1/Qhta.OpenXmlTools.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("OpenXmlTools is a library for working with OpenXml documents.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0+d99765e566f9715c6b2af75319d2dbfa3d10ee09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0+bde8f21fadaf22e204e485a7b21187c156206fda")]
 [assembly: System.Reflection.AssemblyProductAttribute("Qhta.OpenXmlTools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Qhta.OpenXmlTools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("2.0.0.0")]
