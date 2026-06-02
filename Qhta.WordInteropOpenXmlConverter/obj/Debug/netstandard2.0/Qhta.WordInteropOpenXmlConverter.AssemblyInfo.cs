65bb50e81f49a6ffb47beea6551371a11f210870
Jarosław Kuchta <qhta123@outlook.com>
2026-06-02 16:10:11 +0200

    Methods merged to base classes files

--- Qhta.WordInteropOpenXmlConverter/obj/Debug/netstandard2.0/Qhta.WordInteropOpenXmlConverter.AssemblyInfo.cs
+++ Qhta.WordInteropOpenXmlConverter/obj/Debug/netstandard2.0/Qhta.WordInteropOpenXmlConverter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Qhta.WordInteropOpenXmlConverter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5f47b008a83b5cacad81bee088d548f49a877d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19c2867bec2a690dca629385bde22249401c1f19")]
 [assembly: System.Reflection.AssemblyProductAttribute("Qhta.WordInteropOpenXmlConverter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Qhta.WordInteropOpenXmlConverter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
