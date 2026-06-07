7529734c1cece48110998d43fec1489b4844a715
Jarosław Kuchta <qhta123@outlook.com>
2026-06-07 21:20:53 +0200

    DocumentModel.Interop.Enums Word order

--- Qhta.OpenXMLTools/obj/Debug/net4.8/Qhta.OpenXmlTools.AssemblyInfo.cs
+++ Qhta.OpenXMLTools/obj/Debug/net4.8/Qhta.OpenXmlTools.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("OpenXmlTools is a library for working with OpenXml documents.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0+6ba9387162777f1db4bad62f5a4eb752ab66418a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0+be1f09c94f2309a655c8234183464a2194f27f31")]
 [assembly: System.Reflection.AssemblyProductAttribute("Qhta.OpenXmlTools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Qhta.OpenXmlTools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("2.0.0.0")]
