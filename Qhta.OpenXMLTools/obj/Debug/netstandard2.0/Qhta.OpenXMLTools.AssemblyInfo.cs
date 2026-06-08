196f15af102b491f7a9f48172c393b2c9cb02146
Jarosław Kuchta <qhta123@outlook.com>
2026-06-08 10:47:17 +0200

    DocumentModel.Interop.Enum Core order

--- Qhta.OpenXMLTools/obj/Debug/netstandard2.0/Qhta.OpenXMLTools.AssemblyInfo.cs
+++ Qhta.OpenXMLTools/obj/Debug/netstandard2.0/Qhta.OpenXMLTools.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("OpenXmlTools is a library for working with OpenXml documents.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0+be1f09c94f2309a655c8234183464a2194f27f31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0+7529734c1cece48110998d43fec1489b4844a715")]
 [assembly: System.Reflection.AssemblyProductAttribute("Qhta.OpenXmlTools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Qhta.OpenXmlTools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("2.0.0.0")]
