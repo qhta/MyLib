a1d6744d5bab97d48dd687a45dd98aef2c38764b
Jarosław Kuchta <qhta123@outlook.com>
2026-06-08 18:42:49 +0200

    DocumentModel.InOpenXml test revived

--- Qhta.OpenXMLTools/obj/Debug/netstandard2.1/Qhta.OpenXmlTools.AssemblyInfo.cs
+++ Qhta.OpenXMLTools/obj/Debug/netstandard2.1/Qhta.OpenXmlTools.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("OpenXmlTools is a library for working with OpenXml documents.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0+196f15af102b491f7a9f48172c393b2c9cb02146")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0+22494a3a347fa2bfde4e044264f5fcd765814c83")]
 [assembly: System.Reflection.AssemblyProductAttribute("Qhta.OpenXmlTools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Qhta.OpenXmlTools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("2.0.0.0")]
