744b7087f977a6e525b09ad68a38e6dec66ce487
Jarosław Kuchta <qhta123@outlook.com>
2026-06-11 08:01:27 +0200

    CoreProperties, ContentProperties, StatisticProperties & CustomProperties test passed

--- Qhta.OpenXMLTools/obj/Debug/netstandard2.1/Qhta.OpenXmlTools.AssemblyInfo.cs
+++ Qhta.OpenXMLTools/obj/Debug/netstandard2.1/Qhta.OpenXmlTools.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("OpenXmlTools is a library for working with OpenXml documents.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0+a1d6744d5bab97d48dd687a45dd98aef2c38764b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0+6d1175b09495f29534ad844152afeafabe4fd495")]
 [assembly: System.Reflection.AssemblyProductAttribute("Qhta.OpenXmlTools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Qhta.OpenXmlTools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("2.0.0.0")]
