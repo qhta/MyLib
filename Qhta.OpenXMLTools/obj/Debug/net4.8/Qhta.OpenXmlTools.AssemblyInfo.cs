0eb7e44945da17dddb1ac9b6b6c6d282fceb28ad
Jarosław Kuchta <qhta123@outlook.com>
2026-06-20 12:54:58 +0200

    Xml serialization ambiguous type names fixed

--- Qhta.OpenXMLTools/obj/Debug/net4.8/Qhta.OpenXmlTools.AssemblyInfo.cs
+++ Qhta.OpenXMLTools/obj/Debug/net4.8/Qhta.OpenXmlTools.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("OpenXmlTools is a library for working with OpenXml documents.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0+c1e306d703134671730346baec641fdf146b07d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0+f0e81c4dd325a19e886ffa11866faba2496e2124")]
 [assembly: System.Reflection.AssemblyProductAttribute("Qhta.OpenXmlTools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Qhta.OpenXmlTools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("2.0.0.0")]
