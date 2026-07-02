557a900e24016c02be983a71a307a10e4994bb44
Jarosław Kuchta <qhta123@outlook.com>
2026-07-02 21:47:46 +0200

    PixelsMeasure type added

--- Qhta.OpenXMLTools/FootnotePropertiesTools.cs
+++ Qhta.OpenXMLTools/FootnotePropertiesTools.cs
@@ -5,114 +5,112 @@
 /// </summary>
 public static class FootnotePropertiesTools
 {
-  /// <summary>
-  /// Gets the FootnotePosition element value.
-  /// </summary>
   /// <param name="element"></param>
-  /// <returns></returns>
-  public static DXW.FootnotePositionValues? GetFootnotePosition(this DXW.FootnoteProperties element)
+  extension(DXW.FootnoteProperties element)
   {
-    return element.GetFirstElement<DXW.FootnotePosition>()?.Val?.Value;
-  }
-
-  /// <summary>
-  /// Sets the FootnotePosition element value.
-  /// </summary>
-  /// <param name="element"></param>
-  /// <param name="value"></param>
-  public static void SetFootnotePosition(this DXW.FootnoteProperties element, DXW.FootnotePositionValues? value)
-  {
-    if (value == null)
-      element.RemoveAllChildren<DXW.FootnotePosition>();
-    else
+    /// <summary>
+    /// Gets the FootnotePosition element value.
+    /// </summary>
+    /// <returns></returns>
+    public DXW.FootnotePositionValues? GetFootnotePosition()
     {
-      var footnotePosition = element.GetFirstElement<DXW.FootnotePosition>();
-      if (footnotePosition == null)
-      {
-        footnotePosition = new DXW.FootnotePosition();
-        element.AppendChild(footnotePosition);
-      }
-      footnotePosition.Val = value;
+      return element.GetFirstElement<DXW.FootnotePosition>()?.Val?.Value;
     }
-  }
 
-  /// <summary>
-  /// Gets the first NumberingFormat element value
-  /// </summary>
-  /// <param name="element"></param>
-  /// <returns></returns>
-  public static DXW.NumberingFormat? GetNumberingFormat(this DXW.FootnoteProperties element)
-  {
-    return element.GetFirstElement<DXW.NumberingFormat>();
-  }
-
-  /// <summary>
-  /// Sets the NumberingFormat element value.
-  /// </summary>
-  /// <param name="element"></param>
-  /// <param name="value"></param>
-  public static void SetNumberingFormat(this DXW.FootnoteProperties element, DXW.NumberingFormat? value)
-  {
-    element.RemoveAllChildren<DXW.NumberingFormat>();
-    if (value == null)
-      element.AppendChild(value);
-  }
-
-  /// <summary>
-  /// Gets the NumberingStart element value.
-  /// </summary>
-  public static int? GetNumberingStart(this DXW.FootnoteProperties element)
-  {
-    return element.GetFirstElement<DXW.NumberingStart>()?.Val?.Value;
-  }
-
-  /// <summary>
-  /// Sets the NumberingStart element value.
-  /// </summary>
-  /// <param name="element"></param>
-  /// <param name="value"></param>
-  public static void SetNumberingStart(this DXW.FootnoteProperties element, int? value)
-  {
-    if (value == null)
-      element.RemoveAllChildren<DXW.NumberingStart>();
-    else
+    /// <summary>
+    /// Sets the FootnotePosition element value.
+    /// </summary>
+    /// <param name="value"></param>
+    public void SetFootnotePosition(DXW.FootnotePositionValues? value)
     {
-      var numberingStart = element.GetFirstElement<DXW.NumberingStart>();
-      if (numberingStart == null)
+      if (value == null)
+        element.RemoveAllChildren<DXW.FootnotePosition>();
+      else
       {
-        numberingStart = new DXW.NumberingStart();
-        element.AppendChild(numberingStart);
+        var footnotePosition = element.GetFirstElement<DXW.FootnotePosition>();
+        if (footnotePosition == null)
+        {
+          footnotePosition = new DXW.FootnotePosition();
+          element.AppendChild(footnotePosition);
+        }
+        footnotePosition.Val = value;
       }
-      numberingStart.Val = new DX.UInt16Value((ushort)value);
     }
-  }
 
-  /// <summary>
-  /// Gets the NumberingRestart element value.
-  /// </summary>
-  public static DXW.RestartNumberValues? GetNumberingRestart(this DXW.FootnoteProperties element)
-  {
-    return element.GetFirstElement<DXW.NumberingRestart>()?.Val?.Value;
-  }
-
-  /// <summary>
-  /// Sets the NumberingRestart element value.
-  /// </summary>
-  /// <param name="element"></param>
-  /// <param name="value"></param>
-  public static void SetNumberingRestart(this DXW.FootnoteProperties element, DXW.RestartNumberValues? value)
-  {
-    if (value == null)
-      element.RemoveAllChildren<DXW.NumberingRestart>();
-    else
+    /// <summary>
+    /// Gets the first NumberingFormat element value
+    /// </summary>
+    /// <returns></returns>
+    public DXW.NumberingFormat? GetNumberingFormat()
     {
-      var numberingRestart = element.GetFirstElement<DXW.NumberingRestart>();
-      if (numberingRestart == null)
+      return element.GetFirstElement<DXW.NumberingFormat>();
+    }
+
+    /// <summary>
+    /// Sets the NumberingFormat element value.
+    /// </summary>
+    /// <param name="value"></param>
+    public void SetNumberingFormat(DXW.NumberingFormat? value)
+    {
+      element.RemoveAllChildren<DXW.NumberingFormat>();
+      if (value == null)
+        element.AppendChild(value);
+    }
+
+    /// <summary>
+    /// Gets the NumberingStart element value.
+    /// </summary>
+    public int? GetNumberingStart()
+    {
+      return element.GetFirstElement<DXW.NumberingStart>()?.Val?.Value;
+    }
+
+    /// <summary>
+    /// Sets the NumberingStart element value.
+    /// </summary>
+    /// <param name="value"></param>
+    public void SetNumberingStart(int? value)
+    {
+      if (value == null)
+        element.RemoveAllChildren<DXW.NumberingStart>();
+      else
       {
-        numberingRestart = new DXW.NumberingRestart();
-        element.AppendChild(numberingRestart);
+        var numberingStart = element.GetFirstElement<DXW.NumberingStart>();
+        if (numberingStart == null)
+        {
+          numberingStart = new DXW.NumberingStart();
+          element.AppendChild(numberingStart);
+        }
+        numberingStart.Val = new DX.UInt16Value((ushort)value);
       }
-      numberingRestart.Val = value;
+    }
+
+    /// <summary>
+    /// Gets the NumberingRestart element value.
+    /// </summary>
+    public DXW.RestartNumberValues? GetNumberingRestart()
+    {
+      return element.GetFirstElement<DXW.NumberingRestart>()?.Val?.Value;
+    }
+
+    /// <summary>
+    /// Sets the NumberingRestart element value.
+    /// </summary>
+    /// <param name="value"></param>
+    public void SetNumberingRestart(DXW.RestartNumberValues? value)
+    {
+      if (value == null)
+        element.RemoveAllChildren<DXW.NumberingRestart>();
+      else
+      {
+        var numberingRestart = element.GetFirstElement<DXW.NumberingRestart>();
+        if (numberingRestart == null)
+        {
+          numberingRestart = new DXW.NumberingRestart();
+          element.AppendChild(numberingRestart);
+        }
+        numberingRestart.Val = value;
+      }
     }
   }
 }
